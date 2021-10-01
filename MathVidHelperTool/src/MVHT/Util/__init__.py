3ae0ae5096e2d962512bcc87c9e50cb3f77eec4a
Glacier-Programs <51843802+Glacier-Programs@users.noreply.github.com>
2021-09-30 19:41:19 -0500

    Just some files

---
+++ MathVidHelperTool/src/MVHT/Util/__init__.py
@@ -0,0 +1,2 @@
+from .settings import *
+from .Global import *
---
+++ MathVidHelperTool/src/MVHT/Util/settings.py
@@ -0,0 +1,6 @@
+from .color import OFFBLACK, WHITE
+
+BASICBACKGROUNDCOL : tuple[int] = OFFBLACK
+DEFAULTFONTCOLOR : tuple[int] = WHITE
+
+MAXFPS : int = 60
