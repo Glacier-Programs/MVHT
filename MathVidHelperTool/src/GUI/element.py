30052ae2121931fdc96de97dc6e98626c9d16a9a
Glacier-Programs <51843802+Glacier-Programs@users.noreply.github.com>
2021-11-11 17:24:28 -0600

    Update element.py
    Added more functionality

--- MathVidHelperTool/src/GUI/element.py
+++ MathVidHelperTool/src/GUI/element.py
@@ -7,15 +7,16 @@ from MVHT.Util.Global import get_var
 
 '''
     Base classes for element functionality
-    A lot of redudant metopds just to allow for overriding
+    A lot of redudant metods just to allow for overriding
 '''
 
 class Element:
-    def __init__(self, coords : list[int], width : int, height : int, c : tuple[int]= BLACK) -> None:
+    def __init__(self, coords : list[int], width : int, height : int, c : tuple[int]= BLACK, **tags) -> None:
         self.coords = coords
         self.width = width
         self.height = height
         self.c = c
+        self.tags = tags
 
         self.sprite = Surface((width, height))
         self.sprite.fill(c)
@@ -27,33 +28,36 @@ class Element:
     def size(self) ->  tuple[int, int]:
         return self.width, self.height
     
-    def get_col(self) -> bool:
-        mcoords : tuple[int] = get_var('mcoords')
-        xcase : bool = (self.coords[0] < mcoords[0]) and (self.coords[0] + self.width > mcoords[0])
-        ycase : bool = (self.coords[1] < mcoords[1]) and (self.coords[1] + self.height > mcoords[1])
+    def get_col(self, coords :tuple[int]) -> bool:
+        xcase : bool = (self.coords[0] < coords[0]) and (self.coords[0] + self.width > coords[0])
+        ycase : bool = (self.coords[1] < coords[1]) and (self.coords[1] + self.height > coords[1])
         return xcase and ycase
 
     def get_extra_info(self) -> str:
+        pass
+
+    def push_message(self, msg : str) -> None:
         pass
 
     def render(self, surf : Surface) -> None:
         surf.blit(self.sprite, self.coords)
 
 class Button(Element):
-    def __init__(self, coords : list[int], width : int, height : int, c : tuple[int]=BLACK, on_click : Callable[[], None] = False) -> None:
-        super().__init__(coords, width, height, c=c)
-        if on_click:
-            self.on_click = on_click
+    def __init__(self, coords : list[int], width : int, height : int, c : tuple[int]=BLACK, onClick : Callable[[], None] = False, **tags) -> None:
+        super().__init__(coords, width, height, c=c, **tags)
+        if onClick:
+            self.onClick = onClick
     
     def get_extra_info(self) -> str:
         return super().get_extra_info()
 
-    def on_click(self):
-        pass
+    def on_click(self, mcoords : tuple[int]) -> None:
+        if self.onClick:
+            self.onClick()
 
 class AnimatedElement(Element):
-    def __init__(self, coords : list[int], width : int, height : int, c : tuple[int]=BLACK, stepped : bool=False) -> None:
-        super().__init__(coords, width, height, c=c)
+    def __init__(self, coords : list[int], width : int, height : int, c : tuple[int]=BLACK, stepped : bool=False, **tags) -> None:
+        super().__init__(coords, width, height, c=c, **tags)
     
     def get_extra_info(self) -> str:
         return super().get_extra_info()
@@ -67,8 +71,8 @@ class AnimatedElement(Element):
 
 class HoverElement(AnimatedElement):
     # an element that can be hovered over
-    def __init__(self, coords: list[int], width: int, height: int, c: tuple[int] = BLACK, stepped=False) -> None:
-        super().__init__(coords, width, height, c=c, stepped=stepped)
+    def __init__(self, coords: list[int], width: int, height: int, c: tuple[int] = BLACK, stepped=False, **tags) -> None:
+        super().__init__(coords, width, height, c=c, stepped=stepped, **tags)
         self.hoverSprite = Surface((width, height))
         self.hoverSprite.fill((255,255,255))
     
@@ -82,8 +86,14 @@ class HoverElement(AnimatedElement):
         pass
 
 class Text(Element):
-    def __init__(self, coords: list[int], width: int, height: int, c: tuple[int] = BLACK) -> None:
-        super().__init__(coords, width, height, c=c)
+    def __init__(self, coords: list[int], text : str, font : Font, fc: tuple[int] = BLACK, **tags) -> None:
+        self.text = text
+        super().__init__(coords, 0, 0, c=fc, **tags)
+        self.sprite = font.render(self.text, True, fc)
+    
+    def get_col(self) -> bool:
+        ''' collision shouldn't be detected on text. This insures that'''
+        return False
     
     @classmethod
     def make_font(cls, fontName : str, size : int, isSys = True) -> Font:
