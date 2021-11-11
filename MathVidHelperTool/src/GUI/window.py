8796a1eed7959169e19a6e83687711e4985a5cd3
Glacier-Programs <51843802+Glacier-Programs@users.noreply.github.com>
2021-11-11 17:25:25 -0600

    Update window.py
    Fixed several issues. Elements can be added with "priority" to make window events interact with them first

--- MathVidHelperTool/src/GUI/window.py
+++ MathVidHelperTool/src/GUI/window.py
@@ -4,8 +4,9 @@ from pygame import quit as pgQuit
 from pygame.time import Clock
 from typing import NoReturn
 
-from GUI.deeperElements import Button, AnimatedElement, Element, HoverElement
+from GUI.deeperElements import Button, AnimatedElement, Element, Frame, HoverElement
 from GUI.dropDown import DropdownPopup
+from GUI.workpace import WorkspaceFrame
 from MVHT.Util.settings import BASICBACKGROUNDCOL, MAXFPS
 from MVHT.Util import Global
 
@@ -15,6 +16,7 @@ class Window:
         display.set_caption(title)
         self.title = title
         self.elements = {'buttons' : [], 'static' : [], 'animated' : [], 'hover' : [], 'hovering' : []}
+        self.scoped_element = self
         self.CLOCK = Clock()
 
         Global.make_global('mcoords', (0,0))
@@ -26,17 +28,21 @@ class Window:
     @property
     def center(self) -> tuple[int]:
         size = self.size
-        return [size[0]/2, size/2]
+        return [size[0]/2, size[1]/2]
 
     def center_element(self, element : Element) -> tuple[int]:
         ''' return the coordinates needed to center an element on screen '''
-        pass
-
-    def add_element(self, el : Element) -> None:
-        self._add_element(el)
+        center = self.center
+        return [center[0]-element.width/2, center[1]-element.height/2]
+        
+    def add_element(self, el : Element, priority : bool = False) -> None:
+        self._add_element(el, priority)
 
     def remove_element(self, el : Element) -> None:
         self._remove_element(el)
+
+    def push_message(self, msg : str) -> None:
+        pass
 
     def on_close(self) -> None:
         ''' is meant to be overidden by subclasses '''
@@ -46,13 +52,25 @@ class Window:
         while True:
             self._loop()
 
-    def _add_element(self, el : Element) -> None:
-        if isinstance(el, Button):
+    def set_scoped_element(self, el : Element) -> None:
+        self.scoped_element = el
+
+    def _add_element(self, el : Element, priority : bool) -> None:
+        if isinstance(el, Button) and not priority:
             self.elements['buttons'].append(el)
-        if isinstance(el, AnimatedElement):
+        elif isinstance(el, Button) and priority:
+            self.elements['buttons'].insert(0, el)
+        
+        if isinstance(el, AnimatedElement) and not priority:
             self.elements['animated'].append(el)
-        if isinstance(el, HoverElement):
+        elif isinstance(el, AnimatedElement) and priority:
+            self.elements['animated'].insert(0, el)
+
+        if isinstance(el, HoverElement) and not 'no_hover' in el.tags and not priority:
             self.elements['hover'].append(el)
+        elif isinstance(el, HoverElement) and priority and not 'no_hover' in el.tags:
+            self.elements['hover'].insert(0, el)
+
         self.elements['static'].append(el)
 
     def _remove_element(self, el : Element) -> None:
@@ -74,17 +92,25 @@ class Window:
             if ev.type == QUIT:
                 self.on_close()
                 self._shutdown()
+            elif ev.type == KEYDOWN:
+                if ev.unicode == 'p':
+                    print(self.elements)
+                self.scoped_element.push_message(ev.unicode)
             elif ev.type == MOUSEBUTTONDOWN and ev.button == 1:
                 Global.set_var('mcoords', mouse.get_pos())
                 for btn in self.elements['buttons']:
-                    if btn.get_col():
-                        btn.on_click()
+                    if btn.get_col(Global.get_var('mcoords')):
+                        btn.on_click(Global.get_var('mcoords'))
+                        self.scoped_element = btn
+                        if isinstance(btn, Frame):
+                            self.scoped_element = btn.get_pressed_sub(Global.get_var('mcoords'))
+                        break # originally meant to only press one button at a time. Now it has the bonus effect of stopping an infinite loop
 
         Global.set_var('mcoords', mouse.get_pos())
         for hover in self.elements['hover']:
-            intersect = hover.get_col()
+            intersect = hover.get_col(Global.get_var('mcoords'))
             if intersect and not hover in self.elements['hovering']: # they interesct but arent already in the list
-                hover.on_hover()
+                hover.on_hover(Global.get_var('mcoords'))
                 self.elements['hovering'].append(hover)
             elif not intersect and hover in self.elements['hovering']: # the exact opposite of the last if
                 self.elements['hovering'].remove(hover) # remove it from list of hovered objects
@@ -94,5 +120,8 @@ class Window:
         self.display.fill(BASICBACKGROUNDCOL)
         for key in self.elements:
             for el in self.elements[key]:
+                if isinstance(el, WorkspaceFrame):
+                    el.arender(self.display)
+                    continue
                 el.render(self.display)
         display.flip()
