d25e4776c27b002e76d847498139ccd8dded5741
Glacier-Programs <51843802+Glacier-Programs@users.noreply.github.com>
2021-11-11 17:19:52 -0600

    Update __main__.py
    Cleaned up code and started implementing project workspaces

--- MathVidHelperTool/src/__main__.py
+++ MathVidHelperTool/src/__main__.py
@@ -1,26 +1,44 @@
+from MVHT.Project.project import Project
 from MVHT.Util import Global, color
 
 import GUI
+from GUI.workpace import *
 from GUI.deeperElements import *
 from GUI.dropDown import *
 
-def _create_elements():
+import popups
+import cust_workspaces
+
+def cancel_pop_up_decorator(popUp : Popup) -> None:
+    ''' lowers the previous popup before popping up a new one'''
+    if Global.get_var('popup'):
+        Global.get_var('popup').pop_down()
+    Global.set_var('popup', popUp)
+    Global.get_var('popup').pop_up()
+
+def _create_drop_downs() -> None:
     WINSIZE = Global.get_var('WINDOW').size
-    
-    FONT = Text.make_font('arial', 20)
-    Global.make_global('FONT',FONT)
 
     topFrame = Frame([0,0], WINSIZE[0], 25, c=color.GREY)
 
-    redBtn = Button([0,0], 75, 25, c=(255,0,0), on_click=lambda : print('clicked red button'))
-
+    # make all drop downs
     projectMenu = DropdownMenu([0,0], 75, 25, c=(177,177,177), title='Project', font=Global.get_var('FONT'))
     physicsMenu = DropdownMenu([100,0], 75, 25, c=(177,177,177), title='Physics', font=Global.get_var('FONT'))
     mathMenu = DropdownMenu([200,0], 75, 25, c=(177,177,177), title='Math', font=Global.get_var('FONT'))
     variableMenu = DropdownMenu([300,0], 75, 25, c=(177,177,177), title='Variables', font=Global.get_var('FONT'))
-    extensionsMenu = DropdownMenu([400,0], 75, 25, c=(177,177,177), title='Extensions', font=Global.get_var('FONT'))
+    extensionsMenu = DropdownMenu([400,0], 80, 25, c=(177,177,177), title='Extensions', font=Global.get_var('FONT'))
+    
+    # projects
+    projectMenu.popUp.add_option('New', Global.get_var('FONT'), popups.create_new_project_pop_up().pop_up)
+    projectMenu.popUp.add_option('Load', Global.get_var('FONT'), popups.load_project_pop_up().pop_up)
+    projectMenu.popUp.add_option('Save', Global.get_var('FONT'), lambda : print('clicked Save Project'))
+    
+    # physics
+    physicsMenu.popUp.add_option('New System', Global.get_var('FONT'), lambda : print('clicked New Physics System'))
+    physicsMenu.popUp.add_option('New', Global.get_var('FONT'), lambda : print(str(physicsMenu.popUp)))
 
-    projectMenu.popUp.add_option('New', Global.get_var('FONT'), lambda : print('clicked red button'))
+    # math
+    mathMenu.popUp.add_option('New', Global.get_var('FONT'), lambda : print('clicked New Project'))
 
     topFrame.add_element(projectMenu)
     topFrame.add_element(physicsMenu)
@@ -31,12 +49,29 @@ def _create_elements():
     Global.get_var('WINDOW').add_element(topFrame)
     Global.make_global('TOPFRAME',topFrame)
 
+def _create_elements() -> None:
+    WINSIZE = Global.get_var('WINDOW').size
+    _create_drop_downs()
+    WorkspaceFrame.set_workspaces(cust_workspaces.wrkspcs)
+    work_space_frame = WorkspaceFrame([0,25], WINSIZE[0]/2, 500, c = color.BLUE, no_hover=True)
+    Global.set_var('workspace-frame', work_space_frame)
+    Global.get_var('WINDOW').add_element(work_space_frame)
+
+def _make_variables() -> None:
+    Global.make_global('popup', None)
+    Global.make_global('workspace-frame', None)
+    Global.make_global('FONT',Text.make_font('arial', 20))
+    Global.make_global('project',None,on_change=lambda: get_var('workspace-frame').change_project(get_var('project')))
+
+def _start_up() -> None:
+    _make_variables()
+    _create_elements()
+
 def main():
     # start stuff
     win = GUI.Window(1000, 800, 'MVHT')
     Global.make_global('WINDOW', win)
-
-    _create_elements()
+    _start_up()
 
     win.run()
 
