e7eb6c95ec4ffd2a572c15e540c569b7590a22c6
Glacier-Programs <51843802+Glacier-Programs@users.noreply.github.com>
2021-11-11 18:08:51 -0600

    Update settings.py

--- MathVidHelperTool/src/MVHT/Util/settings.py
+++ MathVidHelperTool/src/MVHT/Util/settings.py
@@ -1,8 +1,9 @@
 from .color import OFFBLACK, WHITE
+from os import getcwd
 
 BASICBACKGROUNDCOL : tuple[int] = OFFBLACK
 DEFAULTFONTCOLOR : tuple[int] = WHITE
 
 MAXFPS : int = 60
 
-DEFAULTDIRECTORY = "MVHT/Project/"
+DEFAULTPROJECTSDIRECTORY = getcwd() # change this for your own version
